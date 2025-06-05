aff1dc79bcc4a328d7e2ab4a6d5837b66cc52b8b
frn-af <91farhanalfathra@gmail.com>
2025-06-05 14:33:29 +0900

    feat: map is ready broo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,22 @@
 "use client";
-import { CreateForm } from "@/components/create-form";
-import { ResultDialog } from "@/components/result-dialog";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { Data } from "@/lib/db/schema";
-import Link from "next/link";
-import { useState } from "react";
+import Map from "react-map-gl/maplibre"
+import "maplibre-gl/dist/maplibre-gl.css"
 
 export default function Home() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [formData, setFormData] = useState<Data | undefined>();
 
-  const handleOpen = (data: Data) => {
-    setFormData(data);
-    setIsOpen(true);
-  };
-
-  const handleClose = () => {
-    setIsOpen(false);
-  };
 
   return (
-    <main className="h-full max-w-screen-md mx-auto p-10">
-      <div className="w-full flex justify-center items-center h-full p-4">
-        <Card className="w-2/3">
-          <CardHeader className="flex justify-between w-full">
-            <div className="flex justify-between w-full">
-              <h1>Cek IMT Anda</h1>
-              <Link href="/imt">
-                <Button className="capitalize">lihat data lain</Button>
-              </Link>
-            </div>
-          </CardHeader>
-          <CardContent>
-            <CreateForm onSubmitAction={handleOpen} />
-            <ResultDialog isOpen={isOpen} onClose={handleClose} data={formData!} />
-          </CardContent>
-        </Card>
-      </div>
+    <main className="" style={{ width: "100vw", height: "100vh" }}>
+      <Map
+        initialViewState={{
+          longitude: 132.852959,
+          latitude: -2.2846023,
+          zoom: 6.7
+        }}
+        style={{ width: '100%', height: '100%' }}
+        mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+      />
+
     </main>
   );
 }
